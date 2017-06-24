536e821e4d1727b991fa63cd8eff26853fa465ea
igrek <igrek.s@o2.pl>
2017-06-24 20:06:39 +0200

    Added Logback configuration

--- src/main/java/igrek/webdict/HelloController.java
+++ src/main/java/igrek/webdict/HelloController.java
@@ -1,5 +1,7 @@
 package igrek.webdict;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class HelloController {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Value("${ui.hello.message}")
 	private String message;
 	
 	@GetMapping(value = "/", produces = "text/plain")
 	public @ResponseBody
 	String welcome() {
+		logger.info("This is an info message");
 		return message;
 	}
 }
