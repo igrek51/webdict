a52491a044002d305b19e20ddcd1bd29c2f68c4a
igrek <igrek.s@o2.pl>
2017-06-26 22:57:43 +0200

    Controllers improved

--- src/main/java/igrek/webdict/rest/dictentry/EntriesController.java
+++ src/main/java/igrek/webdict/rest/dictentry/EntriesController.java
@@ -3,17 +3,25 @@ package igrek.webdict.rest.dictentry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import igrek.webdict.db.dictentry.DictEntryDao;
 import igrek.webdict.db.dictionary.DictionaryDao;
 import igrek.webdict.model.DictEntry;
+import igrek.webdict.model.dto.DictEntryDTO;
+import igrek.webdict.model.dto.parser.DictEntryDTOParser;
 
-@Controller
+@RestController
 @RequestMapping("/entries")
 class EntriesController {
 	
@@ -35,4 +43,12 @@ class EntriesController {
 		return dictEntryDao.findOne(id).orElse(null);
 	}
 	
+	@PostMapping()
+	@ResponseStatus(HttpStatus.OK)
+	public void create(@RequestBody List<DictEntryDTO> dtos) {
+		for (DictEntryDTO dto : dtos) {
+			DictEntry dictEntry = DictEntryDTOParser.parse(dto);
+			dictEntryDao.save(dictEntry);
+		}
+	}
 }
--- src/main/java/igrek/webdict/rest/dictentry/EntryController.java
+++ src/main/java/igrek/webdict/rest/dictentry/EntryController.java
@@ -5,13 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import igrek.webdict.db.dictentry.DictEntryDao;
 import igrek.webdict.db.dictionary.DictionaryDao;
@@ -19,7 +18,7 @@ import igrek.webdict.model.DictEntry;
 import igrek.webdict.model.dto.DictEntryDTO;
 import igrek.webdict.model.dto.parser.DictEntryDTOParser;
 
-@Controller
+@RestController
 @RequestMapping("/entry")
 class EntryController {
 	
@@ -36,8 +35,7 @@ class EntryController {
 	}
 	
 	@GetMapping("/{id}")
-	public @ResponseBody
-	DictEntryDTO getById(@PathVariable("id") long id) {
+	public DictEntryDTO getById(@PathVariable("id") long id) {
 		return dictEntryDao.
 				findOne(id).
 				map(DictEntryDTOParser::parse).
