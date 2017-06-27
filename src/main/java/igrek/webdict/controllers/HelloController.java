fda071cc15260d4c22372202563b2b9d221c76bb
igrek <igrek.s@o2.pl>
2017-06-27 02:35:18 +0200

    Reformatted code

--- src/main/java/igrek/webdict/controllers/HelloController.java
+++ src/main/java/igrek/webdict/controllers/HelloController.java
@@ -2,7 +2,6 @@ package igrek.webdict.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +15,16 @@ public class HelloController {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@Value("${ui.hello.message}")
-	private String message;
-	
 	@GetMapping(value = "/", produces = "text/plain")
 	public @ResponseBody
 	String welcome() {
 		logger.info("This is an info message");
-		return message;
+		return "hello";
 	}
 	
 	@GetMapping("/jsp")
 	public String welcomeJSP(Map<String, Object> model) {
-		model.put("message", this.message);
+		model.put("message", "hello");
 		return "welcome"; // view welcome
 	}
 }
