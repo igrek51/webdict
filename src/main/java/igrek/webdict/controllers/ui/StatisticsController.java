ca09905ae481e957dfffcfa4e763dffad52c33f0
iszulc <iszulc@relayonit.pl>
2017-08-31 16:11:47 +0200

    Enable viewing advanced statistics in bootstrap style

--- src/main/java/igrek/webdict/controllers/ui/StatisticsController.java
+++ src/main/java/igrek/webdict/controllers/ui/StatisticsController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +16,7 @@ import igrek.webdict.controllers.ui.common.BaseUIController;
 import igrek.webdict.db.rank.RankDao;
 import igrek.webdict.db.userword.UserWordDao;
 import igrek.webdict.db.word.WordDao;
+import igrek.webdict.model.dto.DictionaryStatisticsDTO;
 import igrek.webdict.model.entity.Dictionary;
 import igrek.webdict.model.entity.Rank;
 import igrek.webdict.model.entity.User;
@@ -45,11 +49,27 @@ public class StatisticsController extends BaseUIController {
 		// get session settings
 		Dictionary dictionary = sessionSettings.getDictionary();
 		User user = sessionSettings.getUser();
-		boolean reversed = sessionSettings.isReversedDictionary();
+		
+		List<DictionaryStatisticsDTO> dictStats = new ArrayList<>();
+		dictStats.add(generateDictStats(dictionary, user, false, "en -> pl"));
+		dictStats.add(generateDictStats(dictionary, user, true, "en <- pl"));
+		model.put("dictStats", dictStats);
+		
+		return "statistics";
+	}
+	
+	private DictionaryStatisticsDTO generateDictStats(Dictionary dictionary, User user, boolean reversed, String dictDisplayName) {
+		
+		DictionaryStatisticsDTO dto = new DictionaryStatisticsDTO();
+		
+		// dictionary display name
+		dto.dictDisplayName = dictDisplayName;
+		Map<String, Object> stats = dto.stats;
 		
 		// all user dictionary ranks
 		List<Rank> ranks = rankDao.findByDictionaryAndUser(dictionary, reversed, user);
-		model.put("allWordsCount", ranks.size());
+		long all = ranks.size();
+		stats.put("allWordsCount", all);
 		
 		if (!ranks.isEmpty()) {
 			
@@ -57,27 +77,37 @@ public class StatisticsController extends BaseUIController {
 			long trainedCount = ranks.stream()
 					.filter(rank -> rank.getRankValue() < 0 && rank.getTriesCount() > 0)
 					.count();
-			model.put("trainedCount", trainedCount);
-			
-			// touched words
-			long touchedCount = ranks.stream().filter(rank -> rank.getTriesCount() > 0).count();
-			model.put("touchedCount", touchedCount);
-			
-			// cooling down words
-			long coolingDownCount = ranks.stream()
-					.filter(rank -> rank.getCooldownPenalty() > 0.0)
-					.count();
-			model.put("coolingDownCount", coolingDownCount);
+			putCounAndPercentage(stats, "trained", trainedCount, all);
 			
 			// training in progress words
 			long trainingInProgressCount = ranks.stream()
 					.filter(rank -> rank.getRankValue() > 0)
 					.count();
-			model.put("trainingInProgressCount", trainingInProgressCount);
+			putCounAndPercentage(stats, "trainingInProgress", trainingInProgressCount, all);
+			
+			// touched words
+			long touchedCount = ranks.stream().filter(rank -> rank.getTriesCount() > 0).count();
+			putCounAndPercentage(stats, "touched", touchedCount, all);
+			
+			// cooling down words
+			long coolingDownCount = ranks.stream()
+					.filter(rank -> rank.getCooldownPenalty() > 0.0)
+					.count();
+			putCounAndPercentage(stats, "coolingDown", coolingDownCount, all);
 			
 		}
 		
-		return "statistics";
+		return dto;
+	}
+	
+	private void putCounAndPercentage(Map<String, Object> model, String name, long count, long all) {
+		model.put(name + "Count", count);
+		double percentage = ((double) count * 100) / all;
+		// converting to string always with dot format
+		DecimalFormatSymbols decimalSymbols = DecimalFormatSymbols.getInstance();
+		decimalSymbols.setDecimalSeparator('.');
+		DecimalFormat format = new DecimalFormat("#.##", decimalSymbols);
+		model.put(name + "Percentage", format.format(percentage));
 	}
 	
 }
---
+++ src/main/java/igrek/webdict/model/dto/DictionaryStatisticsDTO.java
@@ -0,0 +1,12 @@
+package igrek.webdict.model.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DictionaryStatisticsDTO {
+	
+	public String dictDisplayName;
+	
+	public Map<String, Object> stats = new HashMap<>();
+	
+}
